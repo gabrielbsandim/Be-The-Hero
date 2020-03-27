7ac1f93fcfa11399291862e21e6127414a8ff5cf
gabrielbsandim <gabriel.bsandim@gmail.com>
2020-03-27 00:20:17 -0300

    frontend

---
+++ backend/src/routes.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const SessionCoontroller = require('./controllers/SessionController');
+const OngController = require('./controllers/OngController');
+const IncidentController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
+
+const routes = express.Router();
+
+routes.post('/Sessions', SessionCoontroller.create);
+
+routes.get('/ongs', OngController.index);
+routes.post('/ongs', OngController.create);
+
+routes.get('/profile', ProfileController.index);
+
+routes.post('/incidents', IncidentController.create);
+routes.get('/incidents', IncidentController.index);
+routes.delete('/incidents/:id', IncidentController.delete);
+
+module.exports = routes;
